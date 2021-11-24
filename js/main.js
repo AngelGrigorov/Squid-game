37b55fef6a9338d3ce270153dd56813b9b7901b2
AngelGrigorov <damage9797@gmail.com>
2021-11-24 20:50:52 +0200

    Implementing the doll

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+
+camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader();
+
+loader.load("../models/scene.gltf", function(gltf){
+    scene.add(gltf.scene);
+    gltf.scene.scale.set(.4, .4, .4);
+})
+
+
+function animate() {
+	renderer.render( scene, camera );
+
+	requestAnimationFrame( animate );
+
+}
+animate();

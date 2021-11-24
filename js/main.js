ce8f40496e39caeb146abe9f832d6c7f9aedf95b
AngelGrigorov <damage9797@gmail.com>
2021-11-24 22:02:20 +0200

    Adding track and creating player class

--- js/main.js
+++ js/main.js
@@ -10,6 +10,21 @@ renderer.setClearColor(0xb7c3f3, 1);
 const light = new THREE.AmbientLight( 0xffffff ); // soft white light
 scene.add( light );
 
+
+//global
+const start_position = 3;
+const end_position = -start_position;
+
+function createCube(size, positionX, rotY = 0, color = 0xfbc851){
+    const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+    const material = new THREE.MeshBasicMaterial( { color: color } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = positionX;
+    cube.rotation.y = rotY;
+    scene.add( cube );
+    return cube;
+}
+
 camera.position.z = 5;
 
 const loader = new THREE.GLTFLoader();
@@ -34,6 +49,40 @@ class Doll{
         gsap.to(this.doll.rotation, {y: 0, duration: .45})
     }
 }
+
+function createTrack(){
+    createCube({w:start_position * 2 + .2, h: 1.5, d:1}, 0, 0, 0xe5a716).position.z = -1;
+    createCube({w:.2, h: 1.5, d:1}, start_position, -.35)
+    createCube({w:.2, h: 1.5, d:1}, end_position, .35)
+    
+}
+createTrack()
+
+class Player{
+    constructor(){
+        const geometry = new THREE.SphereGeometry( .3, 32, 16 );
+        const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+        const sphere = new THREE.Mesh( geometry, material );
+        sphere.position.z = 1
+        sphere.position.x = start_position
+        scene.add( sphere )
+        this.player = sphere
+        this.playerInfo = {
+            positionX: start_position,
+            velocity: 0.1
+        }
+    }
+
+    run(){
+        this.playerInfo.velocity = .03
+    }
+    update(){
+        this.playerInfo.positionX -= this.playerInfo.velocity
+        this.player.position.x = this.playerInfo.positionX
+    }
+}
+const player = new Player()
+
 let doll = new Doll()
 setTimeout(() => {
     doll.lookBackward()
@@ -43,6 +92,7 @@ function animate() {
 	renderer.render( scene, camera );
 
 	requestAnimationFrame( animate );
+    player.update()
 
 }
 animate();

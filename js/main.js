58b2ad0381e6191e7389b71cf48ec58e4abc5045
AngelGrigorov <damage9797@gmail.com>
2021-11-24 21:30:56 +0200

    Adding gsap library to animate the doll smoothly

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+renderer.setClearColor(0xb7c3f3, 1);
+
 const light = new THREE.AmbientLight( 0xffffff ); // soft white light
 scene.add( light );
 
@@ -12,11 +14,30 @@ camera.position.z = 5;
 
 const loader = new THREE.GLTFLoader();
 
-loader.load("../models/scene.gltf", function(gltf){
-    scene.add(gltf.scene);
-    gltf.scene.scale.set(.4, .4, .4);
-})
 
+
+class Doll{
+    constructor(){
+        loader.load("../models/scene.gltf", (gltf) => {
+            scene.add(gltf.scene);
+            gltf.scene.scale.set(.4, .4, .4);
+            gltf.scene.position.set(0, -1, 0);
+            this.doll = gltf.scene;
+        })
+    }
+
+    lookBackward(){
+       //this.doll.rotation.y = -3.15; 
+        gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+    }
+    lookForward(){
+        gsap.to(this.doll.rotation, {y: 0, duration: .45})
+    }
+}
+let doll = new Doll()
+setTimeout(() => {
+    doll.lookBackward()
+}, 1000);
 
 function animate() {
 	renderer.render( scene, camera );
